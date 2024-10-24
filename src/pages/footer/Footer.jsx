08f25529c9c1229eb21737b3f49a90aafd993915
HectorDev04 <hectorrodriguezzzz04@gmail.com>
2024-10-24 01:14:05 -0600

    Footer section

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -7,17 +7,17 @@ const Footer = () => {
         <p>&copy; 2024 Kodigo. Todos los derechos reservados.</p>
         <ul className="list-inline">
           <li className="list-inline-item">
-            <a href="/about" className="text-white">
+            <a href="#" className="text-white">
               Sobre Nosotros
             </a>
           </li>
           <li className="list-inline-item">
-            <a href="/contact" className="text-white">
+            <a href="#" className="text-white">
               Contacto
             </a>
           </li>
           <li className="list-inline-item">
-            <a href="/privacy" className="text-white">
+            <a href="#" className="text-white">
               Política de Privacidad
             </a>
           </li>
