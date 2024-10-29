59d234b31c1877bc00c4ec1ebf05667b3f049be7
HectorDev04 <hectorrodriguezzzz04@gmail.com>
2024-10-28 19:40:48 -0600

    Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,44 @@
-import { NavBar } from './pages/navbar/NavBar'
-import { Register } from './pages/register/Register'
-import { TokenProvider } from './context/TokenContext'
-import { Bootcamps } from './pages/bootcamps/Bootcamps'
-import { HomeContent } from './pages/homecontent/HomeContent'
-import { Login } from './pages/login/Login'
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import './App.css'
+import { NavBar } from "./pages/navbar/NavBar";
+import { Register } from "./pages/register/Register";
+import { TokenProvider } from "./context/TokenContext";
+import { Bootcamps } from "./pages/bootcamps/Bootcamps";
+import { HomeContent } from "./pages/homecontent/HomeContent";
+import { Login } from "./pages/login/Login";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import Footer from "./pages/footer/Footer";
 
 function App() {
-
-
   return (
     <>
-    <BrowserRouter>
-    <TokenProvider>
-    <header className='headerContent '> {/* Contiene el navbar */}
-      <NavBar/>
-  
-     </header>
-     <main className='mainContent container mt-2'> {/* Contendrá las distintas vistas */}
-     <Routes>
-      <Route path='/' element={<HomeContent/>}/> {/* Direccionaría a la landing */}
-      <Route path='/bootcapms' element={<Bootcamps/>}/> {/* Direccionaría a los bootcamps con la petición a la API(pendiente) */}
-      <Route path='/register' element={<Register/>}/> {/* Direcciona al form de registro(completado) */}
-      <Route path='/login' element={<Login/>}/>{/* Direcciona al form de login (completado) */}
-    </Routes>
-     </main>
-     
-     
-    </TokenProvider>
-    </BrowserRouter>
-    
-    
+      <BrowserRouter>
+        <TokenProvider>
+          <header className="headerContent ">
+            {" "}
+            {/* Contiene el navbar */}
+            <NavBar />
+          </header>
+          <main className="mainContent container mt-2">
+            {" "}
+            {/* Contendrá las distintas vistas */}
+            <Routes>
+              <Route path="/" element={<HomeContent />} />{" "}
+              {/* Direccionaría a la landing */}
+              <Route path="/bootcapms" element={<Bootcamps />} />{" "}
+              {/* Direccionaría a los bootcamps con la petición a la API(pendiente) */}
+              <Route path="/register" element={<Register />} />{" "}
+              {/* Direcciona al form de registro(completado) */}
+              <Route path="/login" element={<Login />} />
+              {/* Direcciona al form de login (completado) */}
+            </Routes>
+          </main>
+          <footer className="footerContent">
+            <Footer />
+          </footer>
+        </TokenProvider>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white mt-auto py-3 text-center">
+      <div className="container">
+        <p>&copy; 2024 K-Bootcamps. Todos los derechos reservados.</p>
+        <ul className="list-inline">
+          <li className="list-inline-item">
+            <a href="#" className="text-white">
+              Sobre Nosotros
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="#" className="text-white">
+              Contacto
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="#" className="text-white">
+              Política de Privacidad
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
